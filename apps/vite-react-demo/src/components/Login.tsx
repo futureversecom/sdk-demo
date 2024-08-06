fc4d9ba99793b8b85260c2baf14c206d2b7b0950
RichClayroadCR <rich@clayroadproductions.com>
2024-08-06 09:58:55 +0100

    Adding FV Provider

---
+++ apps/vite-react-demo/src/components/Login.tsx
@@ -0,0 +1,41 @@
+import { Spinner } from '@fv-sdk-demos/ui-shared';
+import { UserSession } from '@futureverse/auth';
+import { useAuth } from '@futureverse/auth-react';
+import { useEffect, useState } from 'react';
+
+export default function Login() {
+  const { authClient } = useAuth();
+  const [signInState, setSignInState] = useState<boolean | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const userStateChange = (user: UserSession) => {
+      console.log('userStateChange', user);
+
+      if (user) {
+        setSignInState(true);
+      }
+      if (!user) {
+        setSignInState(false);
+      }
+    };
+
+    authClient.addUserStateListener(userStateChange);
+    return () => {
+      authClient.removeUserStateListener(userStateChange);
+    };
+  }, [authClient]);
+
+  if (signInState === true) {
+    return <div>Authenticated - Please Close Window</div>;
+  }
+  if (signInState === false) {
+    return <div>Not Authenticated - Please Try Again</div>;
+  }
+  return (
+    <div>
+      <Spinner /> Authenticating...
+    </div>
+  );
+}
