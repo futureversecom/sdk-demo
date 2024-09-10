d834fac0b4d3e7427603f62e1fcd40725e0d5cfc
RichClayroadCR <rich@clayroadproductions.com>
2024-09-10 23:33:49 +0100

    Fix Next Build Error

--- apps/next-demo/src/Providers/config.ts
+++ apps/next-demo/src/Providers/config.ts
@@ -25,6 +25,7 @@ export const getWagmiConfig = async () => {
     authClient,
     ssr: true,
     chains: [mainnet, sepolia, polygonAmoy],
+    //@ts-expect-error - storage is not in the types
     storage: createStorage({
       storage: cookieStorage,
     }),
