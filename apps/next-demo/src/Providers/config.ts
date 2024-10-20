091865707a7036f252dfee27d85f602d500b5192
RichClayroadCR <rich@clayroadproductions.com>
2024-10-20 08:57:26 +0100

    Overriding Wagmi Config

--- apps/next-demo/src/Providers/config.ts
+++ apps/next-demo/src/Providers/config.ts
@@ -1,9 +1,14 @@
-import { createWagmiConfig } from '@futureverse/wagmi-connectors';
+import {
+  createWagmiConfig,
+  xamanWallet,
+  futureverseCustodialWallet,
+} from '@futureverse/wagmi-connectors';
 import { FutureverseAuthClient } from '@futureverse/auth-react/auth';
 import { mainnet, rootPorcini } from 'viem/chains';
 import { QueryClient } from '@tanstack/react-query';
 import { cookieStorage, createStorage } from 'wagmi';
 import { Storage } from '@wagmi/core';
+import { coinbaseWallet, metaMask, walletConnect } from '@wagmi/connectors';
 
 const clientId = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT as string;
@@ -32,10 +37,37 @@ const rootPorciniAlt = {
 };
 
 export const getWagmiConfig = async () => {
+  const custodialAuthOptions = await authClient.queryCustodialOptions();
+
+  const connectors = [
+    metaMask({
+      dappMetadata: {
+        name: 'FuturePass',
+        url: 'https://playground.therootnetwork.com',
+      },
+    }),
+    coinbaseWallet({
+      appName: 'FuturePass',
+      enableMobileWalletLink: true,
+    }),
+    xamanWallet({
+      apiKey: xamanAPIKey,
+      authClient,
+    }),
+    walletConnect({ projectId: walletConnectProjectId }),
+    ...custodialAuthOptions.map(custodialOpt =>
+      futureverseCustodialWallet({
+        authClient,
+        custodialOpt,
+      })
+    ),
+  ];
+
   return createWagmiConfig({
     walletConnectProjectId,
     xamanAPIKey,
     authClient,
+    connectors,
     ssr: true,
     chains: [mainnet, rootPorciniAlt],
     storage: createStorage({
